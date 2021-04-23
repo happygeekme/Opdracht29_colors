48dad498191874cfa37cf01574916fe4ed18a717
Jitske <80577121+happygeekme@users.noreply.github.com>
2021-04-23 12:41:23 +0200

    Add files via upload

---
+++ colors.js
@@ -0,0 +1,28 @@
+let colors = ["yellow", "blue", "red", "orange"];
+// var i = 0
+
+// while(i < (colors.length)) {
+//    console.log(colors[i]);
+//    i++;
+// }
+
+// for(i = 0; i < colors.length; i++) (
+//     console.log(colors[i])
+// );
+
+// colors.forEach(element => console.log(element));
+
+// 1.  Mijn while loop neemt 5 regels in
+//     Mijn for loop neemt 3 regels in
+// 2.  de forEach loop neemt 1 regel in
+// 3.  Arraymethode = overzichtelijker, ivm korter, beter leesbaar
+
+var person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue",
+    hairColor: "black"
+  };
+
+  console.log(person);
